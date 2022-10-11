7ae0af702a9ae8b5e0755e12e2db1aa41e6de90c
nimaye <Nicolas.MAYE@etu.uca.fr>
2022-10-11 09:02:04 +0200

    classe Manager crée + diagramme de classe ajusté

---
+++ Code/Model/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Manager : INotifyPropertyChanged
+    {
+        public IReadOnlyCollection<Inscrit> ListedesInscrits { get; private set };
+        private List<Inscrit> ListetouslesInscrits { get; set } = new List<Inscrit>;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        public Inscrit SelectedInscrits
+        {
+            get => selectedInscrits;
+            set
+            {
+                if(selectedInscrits != value)
+                {
+                    selectedInscrits = value;
+                    OnPropertyChanged(nameof(SelectedInscrits));
+                }
+            }
+        }
+        private Inscrit selectedInscrits;
+
+
+    }
+
+}
