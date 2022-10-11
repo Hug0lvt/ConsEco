078a38a3849a92096d741ad15d6f6c3bd6394a9a
nimaye <Nicolas.MAYE@etu.uca.fr>
2022-10-11 09:30:45 +0200

    classe Banque créer

---
+++ Code/Model/Banque.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Banque
+    {
+        public string Nom { get; private set; }
+        public string UrlSite { get; private set; }
+        public string UrlLogo { get; private set; }
+        public List<Compte> ListeDesComptes { get; private set; }
+
+        public Banque(string nom, string urlSite, string urlLogo)
+        {
+            Nom = nom;
+            UrlSite = urlSite;
+            UrlLogo = urlLogo;
+        }
+
+        private AjouterCompte(Compte compte)
+        {
+            ListeDesComptes.Add(compte);
+        }
+
+        private SupprimerCompte(Compte compte)
+        {
+            foreach Compte in ListeDesComptes
+            {
+                if (compte.nom == ListeDesComptes.Compte.nom)
+                    ListeDesComptes.Compte.Remove(ListeDesComptes);
+            }
+
+        }
+    }
+}
