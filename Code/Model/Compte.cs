cb76b61f2641eb6e4c4124350bb1af48c780c85d
hulivet1 <Hugo.LIVET@etu.uca.fr>
2022-10-11 09:44:40 +0200

    Ajout de la classe Compte sans les méthodes car pas la classe Opération, Echancier et Plannification

---
+++ Code/Model/Compte.cs
@@ -0,0 +1,14 @@
+﻿namespace Model
+{
+    public class Compte
+    {
+        public string Nom { get; private set; }
+        public double Solde { get; private set; }
+
+        public Compte(string nom, double solde)
+        {
+            Nom = nom;
+            Solde = solde;
+        }
+    }
+}
