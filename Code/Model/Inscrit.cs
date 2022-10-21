9f1cf7c25eebb6e2cd635afc6d2b7d7c7552bfc7
hulivet1 <Hugo.LIVET@etu.uca.fr>
2022-10-21 15:32:54 +0200

    Ajout de tests de classe d'exception et améloration de inscrit

--- Code/Model/Inscrit.cs
+++ Code/Model/Inscrit.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 //using Banque;
 
@@ -9,6 +10,56 @@ namespace Model
 {
     public class Inscrit
     {
+
+        public string Id { get; private set; }
+        public string Nom { get; private set; }
+
+        public string Mail
+        {
+            get => mail;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new InvalidMailException(value, "Longueur d'un mail doit être superieur a 0", new ArgumentNullException());
+                }
+                if (!Regex.IsMatch(value, "(@)(.+)"))
+                {
+                    throw new InvalidMailException(value, "Un mail doit contenir le symbole '@'");
+                }
+                mail = value;
+            }
+        }
+        private string mail;
+
+        public string Prenom { get; private set; }
+
+        public string Mdp
+        {
+            get => mdp;
+            set
+            {
+                if (value.Length <= 8)
+                {
+                    throw new InvalidPasswordException(value, "La longeur d'un mot de passe doit être obligatoirement superieure a 8");
+                }
+                if (!Regex.IsMatch(value, "[A-Z]+"))
+                {
+                    throw new InvalidPasswordException(value, "Le mot de passe doit contenir au moins une lettre majuscule");
+                }
+                if (!Regex.IsMatch(value, "[0-9]+"))
+                {
+                    throw new InvalidPasswordException(value, "Le mot de passe doit contenir au moins un chiffre");
+                }
+                mdp = value;
+            }
+        }
+        private string mdp;
+
+        public double SoldeTotal { get; private set; }
+        public Devises Dev { get; private set; }
+        public List<Banque> LesBanques { get; private set; } = new List<Banque>();
+
         public Inscrit(string id, string nom, string mail, string prenom, string mdp, double soldeTotal)
         {
             Id = id;
@@ -28,14 +79,6 @@ namespace Model
             SoldeTotal = soldeTotal;
             LesBanques = lesbanques;
         }
-        public string Id { get; private set; }
-        public string Nom { get; private set; }
-        public string Mail { get; private set; }
-        public string Prenom { get; private set; }
-        public string Mdp { get; private set; }
-        public double SoldeTotal { get; private set; }
-        public Devises Dev { get; private set; }
-        public List<Banque> LesBanques { get; private set; } = new List<Banque>();
 
         public void ajouterBanque(Banque banque)
         {
--- Code/TestsUnitaires/TestUnitCompte.cs
+++ Code/TestsUnitaires/TestUnitCompte.cs
@@ -9,6 +9,7 @@ namespace TestsUnitaires
 {
     public class TestUnitCompte
     {
+
         [Fact]
         public void TestConstructeurCompte()
         {
@@ -24,7 +25,7 @@ namespace TestsUnitaires
         public void testSupprimerBanque()
         {
             Banque bq = new Banque("Crédit Agricole", "https://creditagricole.fr", "https://yt3.ggpht.com/a/AGF-l7_mEfX2eQaGm8GefLOg5ZMRciNw-pESE3gUWg=s900-c-k-c0xffffffff-no-rj-mo");
-            Inscrit i1 = new Inscrit("A1001", "Smith", "smith@gmail.com", "luke", "test", 500);
+            Inscrit i1 = new Inscrit("A1001", "Smith", "smith@gmail.com", "luke", "test20000aA", 500);
             Assert.NotNull(i1.LesBanques);
             i1.ajouterBanque(bq);
             Assert.Contains(bq, i1.LesBanques);
