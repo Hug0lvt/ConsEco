352920f5e135b5f57869016c5f0228b8f9be7fa8
viastolfi <vincent.astolfi@etu.uca.fr>
2022-11-25 11:37:47 +0100

    premier test de route

--- Sources/API/config/Config.php
+++ Sources/API/config/Config.php
@@ -2,7 +2,7 @@
 //prefixe
 $rep=__DIR__.'/../';
 
-$dsn='mysql:host=10.9.0.67;dbname='.getenv("MARIADB_DATABASE");
+$dsn='mysql:host=10.9.0.65;dbname='.getenv("MARIADB_DATABASE");
 $dbname=getenv("MARIADB_DATABASE");
 $usr=getenv("MARIADB_USER");
 $mdp=getenv("MARIADB_PASSWORD");
