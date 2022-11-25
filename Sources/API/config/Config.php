19f6b56233f8ded74f41800de7f0a16aac85a1c4
viastolfi <vincent.astolfi@etu.uca.fr>
2022-11-25 11:16:19 +0100

    ajout des fichiers nécessaire

---
+++ Sources/API/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+//prefixe
+$rep=__DIR__.'/../';
+
+$dsn='mysql:host=10.9.0.67;dbname='.getenv("MARIADB_DATABASE");
+$dbname=getenv("MARIADB_DATABASE");
+$usr=getenv("MARIADB_USER");
+$mdp=getenv("MARIADB_PASSWORD");
+?>
