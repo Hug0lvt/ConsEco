9f0ca4ba9fe7be38d176879166bad69f4a004924
viastolfi <vincent.astolfi@etu.uca.fr>
2022-11-25 11:20:44 +0100

    changement de l'index.php

--- Sources/API/index.php
+++ Sources/API/index.php
@@ -1,3 +1,10 @@
 <?php
-    echo "test";
+
+require_once(__DIR__.'/config/Config.php');
+
+require_once(__DIR__.'/config/Autoload.php');
+Autoload::charger();
+
+// loading a controler
+$cont = new FrontControler();
 ?>
