7e254fbbacb17f3b7ea4c89812265b898ca23536
Vincent ASTOLFI <vincent.astolfi@etu.uca.fr>
2022-11-30 18:36:29 +0100

    Mise à jour de 'Sources/API/config/Config.php'

--- Sources/API/config/Config.php
+++ Sources/API/config/Config.php
@@ -6,6 +6,4 @@ $dsn='mysql:host='.getenv("DB_SERVER").';dbname='.getenv("MARIADB_DATABASE");
 $dbname=getenv("MARIADB_DATABASE");
 $usr=getenv("MARIADB_USER");
 $mdp=getenv("MARIADB_PASSWORD");
-
-echo $dsn;
 ?>
