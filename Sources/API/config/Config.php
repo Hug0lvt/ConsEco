442490d3ec286fa6f7822f17befcb30d326e0315
Vincent ASTOLFI <vincent.astolfi@etu.uca.fr>
2022-12-02 14:02:40 +0100

    test résolution de la base de données

--- Sources/API/config/Config.php
+++ Sources/API/config/Config.php
@@ -2,7 +2,7 @@
 //prefixe
 $rep=__DIR__.'/../';
 
-$dsn='mysql:host='.getenv("DB_SERVER").':3306'.';dbname='.getenv("MARIADB_DATABASE");
+$dsn='mysql:host='.getenv("DB_SERVER").';dbname='.getenv("MARIADB_DATABASE");
 $dbname=getenv("MARIADB_DATABASE");
 $usr=getenv("MARIADB_USER");
 $mdp=getenv("MARIADB_PASSWORD");
