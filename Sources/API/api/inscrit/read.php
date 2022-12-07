c9db235c3bccdfe1abbec9c60e40249dace3449b
viastolfi <vincent.astolfi@etu.uca.fr>
2022-12-07 14:55:10 +0100

    changement du functionnement de l'API afin de la rendre plus utilisable

---
+++ Sources/API/api/inscrit/read.php
@@ -0,0 +1,35 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Inscrit.php';
+
+    $database = new Database();
+    $db = $database->connect();
+    $inscrit = new Inscrit($db);
+
+    $results = $inscrit->read();
+    $num = $results->rowCount();
+
+    if($num > 0){
+        $inscrit_array = array();
+
+        while($row = $results->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+
+            $inscrit_item = array(
+                'id' => $id,
+                'nom' => $nom,
+                'prenom' => $prenom,
+                'mail' => $mail,
+                'mdp' => $mdp
+            );
+            array_push($inscrit_array, $inscrit_item);
+        }
+        
+        echo json_encode($inscrit_array);
+    } else {
+        echo json_encode(array('message' => 'No Inscrit Found'));
+    }
+?>
