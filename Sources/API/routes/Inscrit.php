f1124e311d8a58cce80a9df3658168d1ce11c722
viastolfi <vincent.astolfi@etu.uca.fr>
2022-12-09 15:24:31 +0100

    modification de l'architecture de l'API afin qu'elle soit de type aplication. utilisation du framework slim php et ajout d'une première route

---
+++ Sources/API/routes/Inscrit.php
@@ -0,0 +1,60 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/Inscrit', function(Request $request, Response $response){
+    $query = "SELECT * FROM Inscrit";
+
+    try{
+        $db = new Database();
+        $conn = $db->connect();
+
+        $stmt = $conn->query($query);
+        $inscrits = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+        $db = null;
+        $response->getBody()->write(json_encode($inscrits));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    } catch(PDOException $e){
+        $error = array("message" => $e->getMessage());
+        
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+
+$app->post('/Inscrit/one', function(Request $request, Response $response,array $args){
+    $mail = $request->getParsedBody();
+    $query = 'SELECT * FROM Inscrit WHERE mail=:mail';
+
+    try{
+        $db = new Database();
+        $conn = $db->connect();
+
+        $stmt = $conn->prepare($query);
+        $stmt->bindValue(':mail',$mail, PDO::PARAM_STR);
+
+        $inscrit = $stmt->fetchAll(PDO::FETCH_OBJ);
+        
+        $db = null;
+        $response->getBody()->write(json_encode($inscrit));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(200);
+    } catch(PDOException $e){
+        $error = array("message" => $e->getMessage());
+        
+        $response->getBody()->write(json_encode($error));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(500);
+    }
+});
+?>
