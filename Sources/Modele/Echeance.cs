7e84a403a0648c1316a27ab6afe329d3f1a9330d
nimaye <Nicolas.MAYE@etu.uca.fr>
2023-01-05 14:03:30 +0100

    maj echeancier

--- Sources/Modele/Echeance.cs
+++ Sources/Modele/Echeance.cs
@@ -8,5 +8,31 @@ namespace Model
 {
     public class Echeance
     {
+        public string IntituleOperation { get; private set; }
+
+        public double Montant { get; private set; }
+
+        public DateTime DateOperation { get; private set; }
+
+        public MethodePayement ModePayement { get; private set; }
+
+        public bool IsDebit { get; private set; }
+        public TagOperation Tag { get; private set; }
+
+        [JsonConstructor]
+        public Echeance(string intituleOperation, double montant, DateTime dateOperation, MethodePayement modePayement, TagOperation tag, bool isDebit = true)
+        {
+            IntituleOperation = intituleOperation;
+            Montant = montant;
+            DateOperation = dateOperation;
+            ModePayement = modePayement;
+            IsDebit = isDebit;
+            Tag = tag;
+        }
+
+        public override string ToString()
+        {
+            return IntituleOperation + " " + DateOperation + " " + Montant + " " + ModePayement + " " + IsDebit;
+        }
     }
 }
